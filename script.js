24f9400ca91cfc6e4b21fffeb52277ce8f73e2d2
EleonoraRud <61467087+EleonoraRud@users.noreply.github.com>
2020-05-13 17:20:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function random_color(){
+	const a = Math.floor(Math.random()*256);
+	const b = Math.floor(Math.random()*256);
+	const c = Math.floor(Math.random()*256);
+
+	const rgbBGcolor = "rgb(" + a +"," + b +"," + c + ")";
+	console.log(rgbBGcolor);
+	document.body.style.background = rgbBGcolor;
+}
