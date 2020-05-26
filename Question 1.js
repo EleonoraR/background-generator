9f3500a34c2e0137140c4b1f83aab87405ee2ba5
EleonoraRud <eleonora.rud.purpose@gmail.com>
2020-05-26 07:47:15 -0400

    Testing to add new file to project

---
+++ Question 1.js
@@ -0,0 +1,53 @@
+function clean(){
+
+const unsortedArray = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+const mixedArray = [1,"2","2",2];
+let sortedArray = unsortedArray.sort((a,b)=>a-b);
+
+console.log(sortedArray);
+
+let temporaryArray = [];
+let organizedByTypeArray = [];
+
+for(i=0; i < sortedArray.lenght; i++){
+	
+}
+
+
+
+
+
+const sortArr = () => {
+
+const unsortedArray = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+const mixedArray = [1,"2","2",2];
+let sortedArray = unsortedArray.sort((a,b)=>a-b);
+
+console.log(sortedArray);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
